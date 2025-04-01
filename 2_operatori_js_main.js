df7ee3c0ebd52fadc65b47f5e3586a6fed81d17b
Dario Mauro <sitiwebdm78@gmail.com>
2025-04-01 06:58:28 +0200

    Eseguiti esercizi come da traccia

---
+++ 2_operatori_js_main.js
@@ -0,0 +1,27 @@
+/*ESERCITAZIONE SELFWORK 2 OPERATORI JS -  */
+
+//DICHIARO E INIZIALIZZO LE VARIBILI
+
+    let correnteanno = 2025; // Variabile che contiene l'anno in corso.
+    let nascita = 1952; // Variabile che contiene l'anno di nascita di una persona.
+    let età = correnteanno - nascita;
+
+// RICAVO L'ETA' DELLA PERSONA.
+
+    console.log('La persona ha :' + età, 'anni di età');
+
+// CALCOLO QUANTI ANNI CI VOGLIO PER ARRIVARE A 100.
+
+    let mancanoa100 = 100 - età;
+    console.log(`Mancano per arrivare a 100 anni, ${mancanoa100} anni.`);
+
+// SI PUO' ANCHE UTILIZZARE UNA COSTANTE
+
+    const centoanni = 100;
+    mancanoa100 = centoanni - età;
+    console.log(`Mancano per arrivare a 100 anni,${mancanoa100} anni.`);
+    
+
+    
+    
+ 
